be0fbab634dd408191c2fb22d065c7794e9cd3a0
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-03-25 11:59:15 +0300

    final work completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+
+let cats = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  return cats.concat(name);
+}
+
+function prependCat(name) {
+     return cats.toSpliced(0, 0, name);
+}
+
+function removeLastCat(name){
+    return cats.slice(0,-1)
+}
+
+function removeFirstCat(name){
+   return cats.slice(1)
+}
